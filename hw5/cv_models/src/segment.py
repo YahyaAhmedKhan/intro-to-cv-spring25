bb19ca9c2c82126ba8ddb536e2f1bce85ea4414a
Yahya Ahmed Khan <yahyaahmed.52@gmail.com>
2025-05-21 11:50:26 +0500

    q3 done

--- hw5/cv_models/src/segment.py
+++ hw5/cv_models/src/segment.py
@@ -19,9 +19,24 @@ class SemanticSegmenter:
         ], axis=0).astype(np.uint8)
 
     def detect_and_draw(self, img: np.ndarray) -> np.ndarray:
+        print(img.shape)
+        # print(img.size)
         torch_img = torch.from_numpy(cv.cvtColor(img, cv.COLOR_BGR2RGB) / 255.0).permute(2, 0, 1).float()
         torch_img = self.transforms(torch_img)
         with torch.no_grad():
             result = self.model(torch.stack([torch_img], dim=0))
 
-        raise NotImplementedError("YOUR CODE HERE. Read the docs / use your debugger to inspect what is returned by the model")
+        output = result["out"][0]
+        label_per_pixel = torch.argmax(output, 0)
+        mask = self.color_per_class_bgr[label_per_pixel]
+        mask = cv.resize(mask, (img.shape[1], img.shape[0]), interpolation=cv.INTER_NEAREST)
+        print(mask.shape)
+
+        new_img = cv.addWeighted(img, (1-self.alpha), mask, self.alpha, 0)
+
+        plt_img = cv.cvtColor(new_img, cv.COLOR_BGR2RGB)
+        # plt.imshow(plt_img)
+        # plt.axis("off")
+        # plt.show()
+
+        return new_img
